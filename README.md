9ae05d1cabf6e8d997dc10f5ac7a0a51a416cf81
danielsinai <daniel.sinai.main@gmail.com>
2023-05-09 09:31:45 +0300

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,52 @@
-# gha-auto-discovery
+<img align="right" width="100" height="74" src="https://user-images.githubusercontent.com/8277210/183290025-d7b24277-dfb4-4ce1-bece-7fe0ecd5efd4.svg" />
+
+# GitHub Actions Auto Discovery
+
+This is a Bash script that automates the process of discovering and syncing GitHub Actions with Port. It uses the Port API to create new actions or update existing ones based on your GitHub Actions workflows.
+
+### Prerequisites
+
+Before running the script, you need to ensure that you have the following prerequisites installed on your system:
+
+- `helm`
+- `kubectl`
+- `yq`
+- `jq`
+
+Also, you need to have the following information ready:
+
+- `PORT_CLIENT_ID`: Your Port organization Client ID (required)
+- `PORT_CLIENT_SECRET`: Your Port organization Client Secret (required)
+- `GITHUB_TOKEN`: Your GitHub token with permissions to read contents + workflows (required)
+- `GITHUB_ORG_NAME`: The name of the GitHub organization to sync with (required)
+- `BLUEPRINT_IDENTIFIER`: The identifier of the blueprint to sync the actions into (required)
+- `ACTION_TRIGGER`: CREATE/DAY-2/DELETE (optional, defaults to `CREATE`)
+
+### Usage
+
+```bash
+#!/bin/bash
+export PORT_CLIENT_ID="PORT_CLIENT_ID"
+export PORT_CLIENT_SECRET="PORT_CLIENT_SECRET"
+export GITHUB_TOKEN="GITHUB_TOKEN"
+export GITHUB_ORG_NAME="GITHUB_ORG_NAME"
+export BLUEPRINT_IDENTIFIER="BLUEPRINT_IDENTIFIER"
+export ACTION_TRIGGER="TRIGGER" # optional, defaults to CREATE
+
+curl -s https://raw.githubusercontent.com/port-labs/actions-auto-discovery/main/github-actions/sync.sh | bash
+```
+
+### How it works
+
+1. It checks for the prerequisites mentioned above.
+2. It checks the Port credentials by calling the Port API with the `PORT_CLIENT_ID` and `PORT_CLIENT_SECRET`.
+3. It retrieves the access token from Port that is needed for API calls.
+4. It retrieves a list of all the workflows in the specified GitHub organization using the GitHub API and prints their paths.
+5. It then iterates through each repository in the organization and retrieves the workflows in each repository. 
+6. It parses each workflow to find the inputs specified in the `workflow_dispatch` event and converts them to JSON.
+7. It then creates a JSON object that describes the action and POSTs it to the Port API to either create a new action or update an existing one.
+
+### Limitations
+
+- This script only works for GitHub Actions workflows that use the `workflow_dispatch` event.
+- The script can only sync actions with Port that have been triggered by the `workflow_dispatch` event.
---
+++ github-actions/sync.sh
@@ -0,0 +1,116 @@
+#!/bin/bash
+set -e
+
+#########################################################################################################################################################
+# Description:
+#   This script is responsible for automatically discovering and syncing GitHub Actions with Port.
+# 
+# Variables:
+#   PORT_CLIENT_ID - Your Port organization Client ID (required)
+#   PORT_CLIENT_SECRET - Your Port organization Client Secret (required)
+#   GITHUB_TOKEN - Your GitHub token (required)
+#   GITHUB_ORG_NAME - The name of the GitHub organization to sync with (required)
+#   BLUEPRINT_IDENTIFIER - The identifier of the blueprint to sync with (required)
+#   ACTION_TRIGGER - The trigger to use when syncing actions (optional, default: CREATE)
+#########################################################################################################################################################
+
+# Global variables
+REPO_BRANCH=${REPO_BRANCH:-"main"}
+REPO_BASE_URL="https://raw.githubusercontent.com/port-labs/template-assets/${REPO_BRANCH}"
+COMMON_FUNCTIONS_URL="${REPO_BASE_URL}/common.sh"
+TRIGGER="${ACTION_TRIGGER:-"CREATE"}"
+# Create temporary folder
+function cleanup {
+  rm -rf "${temp_dir}"
+}
+
+trap cleanup EXIT
+
+temp_dir=$(mktemp -d)
+
+echo "Importing common functions..."
+curl -s ${COMMON_FUNCTIONS_URL} -o "${temp_dir}/common.sh"
+source "${temp_dir}/common.sh"
+
+echo "Checking for prerequisites..."
+check_commands "helm" "kubectl" "yq" "jq"
+check_port_credentials "${PORT_CLIENT_ID}" "${PORT_CLIENT_SECRET}"
+
+access_token=$(curl -s --location --request POST 'https://api.getport.io/v1/auth/access_token' --header 'Content-Type: application/json' --data-raw "{
+    \"clientId\": \"${PORT_CLIENT_ID}\",
+    \"clientSecret\": \"${PORT_CLIENT_SECRET}\"
+}" | jq -r '.accessToken')
+
+
+echo "Syncing GitHub Actions with Port..."
+curl -s -H "Authorization: token $GITHUB_TOKEN" -H "Accept: application/json" "https://api.github.com/repos/$GITHUB_ORG_NAME/port/actions/workflows?per_page=100" | jq -r '.workflows[].path'
+
+for repo_name in $(curl -s -H "Authorization: token $GITHUB_TOKEN" "https://api.github.com/orgs/$GITHUB_ORG_NAME/repos" | jq -r '.[].name'); do
+  for file_path in $(curl -s -H "Authorization: token $GITHUB_TOKEN" -H "Accept: application/json" "https://api.github.com/repos/$GITHUB_ORG_NAME/$repo_name/actions/workflows?per_page=100" | jq -r '.workflows[].path'); do
+    echo "Syncing $repo_name/$file_path..."
+    
+    input_yaml_file="$temp_dir/workflow.yaml"
+    output_file="$temp_dir/output.json"
+
+    URL="https://api.github.com/repos/$GITHUB_ORG_NAME/$repo_name/contents/$file_path"
+    echo "Downloading $URL..."
+    
+    response_code=$(curl -w "%{http_code}" -s -o /dev/null -H "Authorization: token $GITHUB_TOKEN" "$URL")
+
+    if [[ ${response_code} == "404" ]]; then
+        echo "Error: $i returned a 404 status code. Exiting loop."
+        continue
+    fi
+
+    curl -s -H "Authorization: token $GITHUB_TOKEN" "$URL" | jq -r '.content' | base64 -d > $input_yaml_file
+
+    if ! yq -e '.on.workflow_dispatch' $input_yaml_file >/dev/null 2>&1; then
+      echo "Error: yq failed to parse workflow_dispatch section in $input_yaml_file. Skipping..."
+      continue
+    fi
+
+    yq -i '.on.workflow_dispatch.inputs' $input_yaml_file
+
+    echo "Parsing workflow_dispatch.inputs to JSON"
+    cat $input_yaml_file
+    yq eval -j $input_yaml_file > $output_file
+
+    if [[ $(cat $output_file) == "null" ]]; then
+      echo "No workflow_dispatch.inputs found. putting an empty object at the action.json file..."
+      echo "{}" > $output_file
+    fi
+
+    
+    jq_filter='{
+      "identifier": "'$repo_name'-'$(basename $file_path .yaml)'",
+      "title": "'$repo_name' '$(basename $file_path .yaml)'",
+      "trigger": "'$TRIGGER'",
+      "invocationMethod": {
+        "type": "GITHUB",
+        "org": "'$GITHUB_ORG_NAME'",
+        "repo": "'$repo_name'",
+        "workflow": "'$(basename $file_path .yaml)'",
+        "omitPayload": true
+      },
+      "userInputs": {
+        "properties": map_values({
+        "description": (.description // null),
+        "type": (.type | if . == "choice" then "string" else . end),
+        "default": (.default // null),
+        "enum": (.options // null)
+      } | with_entries(select(.value != null))),
+        "required": [to_entries[] | select(.value.required == true) | .key]
+      }
+    }'
+
+    jq "$jq_filter" "$output_file" > $temp_dir/action.json
+
+    echo "Syncing action.json to Port..."
+    cat $temp_dir/action.json
+
+    curl --location -X POST "https://api.getport.io/v1/blueprints/$BLUEPRINT_IDENTIFIER/actions" \
+      --header "Authorization: Bearer $access_token" \
+      --header "Content-Type: application/json" \
+      --data-raw "$(cat $temp_dir/action.json)"
+  done
+done
